06f20f8188d45340c9438499dee1dc40fae12dc0
ali-mfahim <sm.ali10@yahoo.com>
2024-10-08 16:03:05 +0500

    fixes

--- config/app.php
+++ config/app.php
@@ -14,6 +14,7 @@ return [
     */
 
     'name' => env('APP_NAME', 'Laravel'),
+    'cron_enable' => env("CRON_ENABLE" , false),
 
     /*
     |--------------------------------------------------------------------------
